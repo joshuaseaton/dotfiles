5ede5b20aeab771c165822697af53c68f23df124
Joshua Seaton <josh.a.seaton@gmail.com>
2024-03-24 14:19:16 -0700

    [bootstrap] Install cargo via official rust installation method
    This makes the bootstrap script more environment agnostic (leaving that
    to the official downloaded rustup script).

--- bootstrap/nu.sh
+++ bootstrap/nu.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 
 #
-# The sole job of this script is to install Nushell (via cargo) and our custom
-# configuration for it, and register it as the default terminal shell.
+# The primary job of this script is to install Nushell (via cargo) and our
+# custom configuration for it, and register it as the default terminal shell.
 #
 
-readonly OS="$(uname -o)"
-readonly NU_ENV_CONFIG="${HOME}/.dotfiles/nu/config/env.nu"
+set -e
 
-if [[ -z "$(which cargo)" ]]; then
-    if [[ "${OS}" == Darwin ]]; then
-        if [[ -z "$(which brew)" ]]; then
-            curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-        fi
-        brew install rust
-    else
-        echo "Unsupported OS: ${OS}"
-        exit 1
-    fi
+BREW=/opt/homebrew/bin/brew
+OS="$(uname -o)"
+NU_ENV_CONFIG="${HOME}/.dotfiles/nu/config/env.nu"
+readonly BREW OS NU_ENV_CONFIG
+
+# This is also the most convenient spot to install Homebrew, which is intended
+# to be done with bash.
+if [ "${OS}" = Darwin ] && [ ! -f "${BREW}" ]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [[ -z "$(which nu)" ]]; then
+if [ ! -f "${HOME}/.cargo/bin/cargo" ]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+. "${HOME}/.cargo/env"
+
+if [ -z "$(which nu)" ]; then
     cargo install nu
 fi
 
 # Now finish our bootstrap in the richer Nushell environment.
-$"${HOME}/.cargo/bin/nu" --env-config "${NU_ENV_CONFIG}" "${HOME}/.dotfiles/bootstrap/nu.nu"
+nu --env-config "${NU_ENV_CONFIG}" "${HOME}/.dotfiles/bootstrap/nu.nu"
--- update.nu
+++ update.nu
@@ -5,17 +5,20 @@ use log.nu
 # VSCode
 run $"($env.DOTFILES)/vscode/update.nu"
 
+# Rust
+
+log newline
+log info $"(ansi wb)Updating Rust installations...(ansi reset)"
+
+^rustup update
+
+# TODO: When we have multiple cargo-managed installations, parse the list from
+# cargo install --list.
+^cargo install nu
+
 # OS-specific set-up.
 match (sys | get host.name) {
     Darwin => (run $"($env.DOTFILES)/macos/update.nu")
 }
 
-# OS-specific set-up will have ensured that cargo is up-to-date, at which point
-# we can update the cargo-managed installations.
-#
-# TODO: When we have multiple cargo-managed installations, parse the list from
-# cargo install --list.
-log newline
-log info $"(ansi wb)Updating cargo installations...(ansi reset)"
-
-^cargo install nu
+exit
