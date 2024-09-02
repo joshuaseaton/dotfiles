93acd6ed6beb582a8cae932c165aa9970aeced36
Joshua Seaton <josh.a.seaton@gmail.com>
2024-09-02 13:28:23 -0700

    [macos] Make Ctrl+Alt+T a hotkey for launching/focusing alacritty

--- system/macos/hammerspoon/init.lua
+++ system/macos/hammerspoon/init.lua
@@ -1,2 +1,6 @@
 -- Auto-generates annotations for all installed Spoons.
 hs.loadSpoon("EmmyLua")
+
+hs.hotkey.bind({ "ctrl", "alt" }, "T", function()
+    hs.application.launchOrFocus("alacritty")
+end)
